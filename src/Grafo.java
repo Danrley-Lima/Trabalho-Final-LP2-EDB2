876244838c87d5fb3848930d6d71ca635120544a
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2022-11-21 15:11:35 -0300

    Lógica inicial de Vertices e Arestas
    - O Grafo tá guardando os vértices e as arestas, sem nenhuma outra checagem.
    - Os vértices estão guardando suas ligações(arestas) com outros vértices

--- src/Grafo.java
+++ src/Grafo.java
@@ -32,12 +32,17 @@ public class Grafo {
 	 * 
 	 * */
 	public void addVertice(int id){
-		return;
+		Vertice novoVertice = new Vertice(id);
+		vertices.add(novoVertice);
 	}
 	
 	/*
 	 * Método para criar uma aresta e add ela à lista
 	 * de arestas do grafo
+	 * 
+	 * CREIO QUE AQUI TEREMOS QUE VERIFICAR SE O GRAFO
+	 * FICA CÍCLICO AO ADD A NOVA ARESTA, MAS ISSO
+	 * ENVOLVE CONJUNTO DISJUNTOS, EU ACHO
 	 * 
 	 * @param vertice1 Vertice que quer fazer a conexão
 	 * @param vertice2 Vertice que vai receber a conexão
@@ -45,6 +50,12 @@ public class Grafo {
 	 * 
 	 * */
 	public void addAresta(Vertice vertice1, Vertice vertice2, int custo){
+		Aresta novaAresta = new Aresta(vertice1, vertice2, custo);
+		arestas.add(novaAresta);
+		
+		//adiciona a ligação nas arestas
+		vertice1.adicionarLigacao(vertice2, custo);
+		vertice2.adicionarLigacao(vertice1, custo);
 		return;
 	}
 	
@@ -54,4 +65,25 @@ public class Grafo {
 	public Arvore gerarArvore(){
 		
 	}*/
+	
+	public List<Vertice> getVertices(){
+		return vertices;
+	}
+	
+	public List<Aresta> getArestas(){
+		return arestas;
+	}
+	
+	//método apenas pra testar
+	public void imprimeLigacoes(){
+		System.out.println("Vertices: ");
+		for(Vertice v:vertices){
+			System.out.println("Casa " + v.getId());
+		}
+		System.out.print("\n\nligações: \n");
+		
+		for(Aresta a:arestas){
+			System.out.println(a.getVertice1() + " " + a.getVertice2() + " Custo: " + a.getCusto());
+		}
+	}
 }
