dc700d578e7fbdb08b41ccf3b21b6a5f55c133da
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2022-11-22 14:54:43 -0300

    Vertice ainda extends

--- src/Vertice.java
+++ src/Vertice.java
@@ -1,19 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Vertice {
+public class Vertice extends ConjuntoDisjunto{
 	private int id;
 	private List<Aresta> ligacoes;
-	private int grau;
 	
 	Vertice(int id){
 		this.id = id;
 		this.ligacoes = new ArrayList<>();
-		this.grau = 0;
+		
+		//Atributos de ConjuntoDisjunto
+		this.MakeSet(this);
+		/*
+		this.ordem = 0;
+		this.representante = this;
+		this.valor = this;
+		*/
 	}
 	
 	public void adicionarLigacao(Vertice destino, int custo){
 		Aresta novaAresta = new Aresta(this, destino, custo);
+		this.ordem++;
+		destino.aumentaOrdem();
 		ligacoes.add(novaAresta);
 	}
 	
@@ -25,8 +33,12 @@ public class Vertice {
 		return ligacoes;
 	}
 	
-	public int getGrau(){
-		return grau;
+	public int getOrdem(){
+		return ordem;
+	}
+	
+	public void aumentaOrdem(){
+		this.ordem++;
 	}
 	
 	@Override
