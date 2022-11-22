2e44d9f429f04bac331ff5785f3086f455ca40c5
Anchel17 <80929766+Anchel17@users.noreply.github.com>
2022-11-22 14:53:45 -0300

    Vertice ainda Extends

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,13 @@ public class Main {
 		grafo.addVertice(1);
 		grafo.addVertice(2);
 		grafo.addVertice(3);
+		grafo.addVertice(4);
 		
 		List<Vertice> vertices = grafo.getVertices();
 		grafo.addAresta(vertices.get(0), vertices.get(1), 8);
-		grafo.addAresta(vertices.get(2), vertices.get(1), 5);
+		grafo.addAresta(vertices.get(2), vertices.get(1), 5);	
+		grafo.addAresta(vertices.get(0) , vertices.get(2), 10);
+		grafo.addAresta(vertices.get(3), vertices.get(0), 7);
 		
 		/*
 		 * Teste fora do grafo para ver se os vertices
@@ -22,6 +25,14 @@ public class Main {
 				System.out.println(a.getVertice1() + " " + a.getVertice2());
 			}
 		}
-		//grafo.imprimeLigacoes();
+		grafo.imprimeLigacoes();
+		
+		//teste de representantes
+		
+		System.out.println("representante da " + vertices.get(0) + " é: " + vertices.get(0).find());
+		System.out.println("representante da " + vertices.get(1) + " é: " + vertices.get(1).find());
+		System.out.println("representante da " + vertices.get(2) + " é: " + vertices.get(2).find());
+		System.out.println("representante da " + vertices.get(3) + " é: " + vertices.get(3).find());
+		
 	}
 }
